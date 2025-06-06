40ebda1d723993f8acea9bf05d52bdb0668e3bc7
TuMankito77 <bolivaritoviva@gmail.com>
2025-06-05 21:47:00 -0400

    TrackSelectionView modification
    - Added a new list for storing the buttons that will be used for selecting the track.

--- RacingGameTechDemoUnity/Assets/Scripts/RacingGameDemo/Runtime/UI/Views/TrackSelectionView.cs
+++ RacingGameTechDemoUnity/Assets/Scripts/RacingGameDemo/Runtime/UI/Views/TrackSelectionView.cs
@@ -1,10 +1,13 @@
 namespace RacingGameDemo.Runtime.UI.Views
 {
+    using System.Collections.Generic;
+    
+    using GameBoxSdk.Runtime.UI.CoreElements;
     using GameBoxSdk.Runtime.UI.Views;
 
     public class TrackSelectionView : BaseView
     {
-        
+        private List<BaseButton> trackButtons = null;
     }
 }
 
