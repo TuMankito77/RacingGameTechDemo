10def59e5285932e09e3ecbc576ccd9061883b99
TuMankito77 <bolivaritoviva@gmail.com>
2025-06-06 19:57:36 -0400

    TrackSelectionView update
    - Added a new parameter that will hold a reference to the parent object that will have all the track buttons.

--- RacingGameTechDemoUnity/Assets/Scripts/RacingGameDemo/Runtime/UI/Views/TrackSelectionView.cs
+++ RacingGameTechDemoUnity/Assets/Scripts/RacingGameDemo/Runtime/UI/Views/TrackSelectionView.cs
@@ -1,12 +1,17 @@
 namespace RacingGameDemo.Runtime.UI.Views
 {
     using System.Collections.Generic;
-    
+
+    using UnityEngine;
+
     using GameBoxSdk.Runtime.UI.CoreElements;
     using GameBoxSdk.Runtime.UI.Views;
 
     public class TrackSelectionView : BaseView
     {
+        [SerializeField]
+        private Transform trackButtonsContainer = null;
+
         private List<BaseButton> trackButtons = null;
     }
 }
