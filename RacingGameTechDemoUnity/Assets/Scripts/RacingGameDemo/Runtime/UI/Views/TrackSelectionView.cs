005aa598a1d708dbf2279cdfc0c7d645e7d279ce
TuMankito77 <bolivaritoviva@gmail.com>
2025-06-09 22:36:48 -0400

    TrackSelectionView update
    - Overrode the TrackSelectionView Initialize method.

--- RacingGameTechDemoUnity/Assets/Scripts/RacingGameDemo/Runtime/UI/Views/TrackSelectionView.cs
+++ RacingGameTechDemoUnity/Assets/Scripts/RacingGameDemo/Runtime/UI/Views/TrackSelectionView.cs
@@ -1,11 +1,15 @@
 namespace RacingGameDemo.Runtime.UI.Views
 {
+    using System;
     using System.Collections.Generic;
 
     using UnityEngine;
+    using UnityEngine.EventSystems;
 
     using GameBoxSdk.Runtime.UI.CoreElements;
     using GameBoxSdk.Runtime.UI.Views;
+    using GameBoxSdk.Runtime.Sound;
+    using GameBoxSdk.Runtime.UI.Views.DataContainers;
 
     public class TrackSelectionView : BaseView
     {
@@ -13,6 +17,11 @@ namespace RacingGameDemo.Runtime.UI.Views
         private Transform trackButtonsContainer = null;
 
         private List<BaseButton> trackButtons = null;
+
+        public override void Initialize(Camera uiCamera, Action<ClipIds> playClipOnce, ViewInjectableData viewInjectableData, Func<string, string> getLocalizedText, EventSystem sourceEventSystem)
+        {
+            base.Initialize(uiCamera, playClipOnce, viewInjectableData, getLocalizedText, sourceEventSystem);
+        }
 
         public override void TransitionIn(int sourceInteractableGroupId)
         {
