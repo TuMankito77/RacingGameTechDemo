92bae34ea1202ef44f9ae976710767ef35ce1b51
TuMankito77 <bolivaritoviva@gmail.com>
2025-06-16 08:32:44 -0400

    Mouse rotation implementation
    - Added the logic needed for making the car showcase view rotate when the player presses the left mouse button and moves its position while the button is pressed.

--- RacingGameTechDemoUnity/Assets/Scripts/RacingGameDemo/Runtime/UI/Views/CarShowcaseViewController.cs
+++ RacingGameTechDemoUnity/Assets/Scripts/RacingGameDemo/Runtime/UI/Views/CarShowcaseViewController.cs
@@ -3,6 +3,7 @@ namespace RacingGameDemo.Runtime.UI.Views
     using System;
     
     using UnityEngine;
+    using UnityEngine.InputSystem;
     
     using GameBoxSdk.Runtime.Input;
 
@@ -13,14 +14,27 @@ namespace RacingGameDemo.Runtime.UI.Views
         private CarShowcaseView carShowCaseView = null;
         private Transform renderCameraTransform = null;
         private float currenPanRotationSpeed = 0;
+        private Vector2? lastMousePosition = null;
 
         public override void Enable(InputActions sourceInputActions, IInputControlableEntity sourceEntityToControl)
         {
             base.Enable(sourceInputActions, sourceEntityToControl);
             carShowCaseView = sourceEntityToControl as CarShowcaseView;
             inputActions.CarShowcaseController.Enable();
+            inputActions.CarShowcaseController.ActivateMouseRotation.started += OnActivateMouseRotationStarted;
+            inputActions.CarShowcaseController.ActivateMouseRotation.canceled += OnActivateMouseRotationCanceled;
             renderCameraTransform = carShowCaseView.CarShowcaseStudio.CameraCenterParentTransform;
             currenPanRotationSpeed = 0;
+        }
+
+        private void OnActivateMouseRotationCanceled(InputAction.CallbackContext inputContext)
+        {
+            lastMousePosition = null;
+        }
+
+        private void OnActivateMouseRotationStarted(InputAction.CallbackContext inputContext)
+        {
+            lastMousePosition = inputActions.CarShowcaseController.MouseRotate.ReadValue<Vector2>();
         }
 
         public override void Update()
@@ -32,7 +46,21 @@ namespace RacingGameDemo.Runtime.UI.Views
 
         private void CalculatePanRotationSpeed()
         {
-            Vector2 panRotationDirection = inputActions.CarShowcaseController.Rotate.ReadValue<Vector2>();
+            Vector2 panRotationDirection = Vector2.zero;
+
+            if(inputActions.CarShowcaseController.ActivateMouseRotation.IsPressed() && lastMousePosition != null)
+            {   
+                Vector2 currentMousePosition = inputActions.CarShowcaseController.MouseRotate.ReadValue<Vector2>();
+                panRotationDirection = currentMousePosition - lastMousePosition.Value;
+                panRotationDirection.Normalize();
+                lastMousePosition = currentMousePosition;
+            }
+            else
+            {
+                panRotationDirection = inputActions.CarShowcaseController.Rotate.ReadValue<Vector2>();
+                panRotationDirection.Normalize();
+            }
+
             float desiredPanRotationSpeed = panRotationDirection.x * carShowCaseView.PanRotationSpeed;
 
             if(currenPanRotationSpeed < desiredPanRotationSpeed)
